1f9bab76eb4619c98b10628e4c041ca74ac84447
hayet <habethayet@gmail.com>
2017-05-31 11:12:40 +0200

    create file

---
+++ create-file.php
@@ -0,0 +1,6 @@
+<?php
+$text = $_GET['textarea']; // récupération de l'adresse envoyée avec le formulaire (ton champ de texte doit s'appeler "email")
+$file = fopen("textarea.txt", "w"); // ouvre le fichier en écriture
+fwrite($file, $text); // ecrit l'e-mail dans le fichier
+fclose($file); // ferme le fichier
+?>
