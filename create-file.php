892a7077aa20d78eae080975b4e5e71b52648450
hayet <habethayet@gmail.com>
2017-05-31 13:55:27 +0200

    posts

--- create-file.php
+++ create-file.php
@@ -3,11 +3,11 @@ $contenu = $_POST["textarea"]; // récupération de l'adresse envoyée avec le f
 echo "<p>".$contenu."</p>";
 $titre = $_POST["titre"];
 echo "<p>".$titre."</p>";
-if(!is_dir("dossier")){
+if(!is_dir("posts")){
 
-mkdir("dossier");
+mkdir("posts");
 }
-$fichier = fopen("dossier/".$titre.".txt", "w");
+$fichier = fopen("posts/".$titre.".txt", "w");
 fwrite($fichier,$contenu);
 fclose($fichier);
 ?>
