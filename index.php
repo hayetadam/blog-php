da1a10c1323f17950252b37e98dacedb4b580423
hayet <habethayet@gmail.com>
2017-06-02 09:53:24 +0200

    php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>index.php</title>
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mon super blog</title>
+</head>
+<body>
+    <h1>Mon supe blog !</h1>
+    <?php
+    $files = scandir("posts");
+    foreach($files as $file) {
+        if (is_dir($file)) {
+            continue;
+        }
+        echo '<h2>'.basename($file, ".txt").'</h2>';
+        $content = file_get_contents('posts/'.$file);
+        echo '<p>'.$content.'</p>';
+        echo '<form  method="GET" action="delete.php">
+       <input type="hidden" name="fileName" value="'.$file.'">
+  <input type="submit" value="delete">
+   </form>';
+    }
+   
+    ?>
+     
+
+        
+</body>
+   
+</html>
